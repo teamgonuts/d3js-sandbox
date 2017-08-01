01584987861a7003d3b87a42481685e5de735ace
Calvin Hawkes <calvinhawkes@gmail.com>
2017-08-01 18:31:09 -0400

    gradient on actual hours of the day done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,32 @@ class PagesController < ApplicationController
   def test_2
   end
   def flickr_time
-    gon.dates = FlickrPhoto.all.limit(10).pluck(:date_taken)
-    # formatted = []
-    # dates.each do |d|
-    #   puts "Old Date: #{d}"
-    #   new_date = d.strftime('1/1/2016 %H:%M')
-    #   formatted <<
-    # end
+    photos = FlickrPhoto.select(:date_taken).group_by {|p| p.date_taken.hour}
+    gon.frequency_map = {
+      0 => photos[0].size,
+      1 => photos[1].size,
+      2 => photos[2].size,
+      3 => photos[3].size,
+      4 => photos[4].size,
+      5 => photos[5].size,
+      6 => photos[6].size,
+      7 => photos[7].size,
+      8 => photos[8].size,
+      9 => photos[9].size,
+      10 => photos[10].size,
+      11 => photos[11].size,
+      12 => photos[12].size,
+      13 => photos[13].size,
+      14 => photos[14].size,
+      15 => photos[15].size,
+      16 => photos[16].size,
+      17 => photos[17].size,
+      18 => photos[18].size,
+      19 => photos[19].size,
+      20 => photos[20].size,
+      21 => photos[21].size,
+      22 => photos[22].size,
+      23 => photos[23].size
+    }
   end
 end
