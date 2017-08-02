8478ddf3ae065b011e74be422dc8ef20ef30b69a
Calvin Hawkes <calvinhawkes@gmail.com>
2017-08-02 14:32:56 -0400

    flickr month created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   def joymap
   end
 
-  def flickr_time
+  def flickr_hour
     photos = FlickrPhoto.where(city_name: 'New York').select(:date_taken).group_by {|p| p.date_taken.hour}
     gon.frequency_map = {
       0 => photos[0].size,
@@ -35,6 +35,23 @@ class PagesController < ApplicationController
       21 => photos[21].size,
       22 => photos[22].size,
       23 => photos[23].size
+    }
+  end
+  def flickr_month
+    photos = FlickrPhoto.where(city_name: 'Santiago').select(:date_taken).group_by {|p| p.date_taken.month}
+    gon.frequency_map = {
+      1 => photos[1].size,
+      2 => photos[2].size,
+      3 => photos[3].size,
+      4 => photos[4].size,
+      5 => photos[5].size,
+      6 => photos[6].size,
+      7 => photos[7].size,
+      8 => photos[8].size,
+      9 => photos[9].size,
+      10 => photos[10].size,
+      11 => photos[11].size,
+      12 => photos[12].size
     }
   end
 end
