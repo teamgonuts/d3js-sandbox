3071e32ee9b4f139b5ea7f1e2199ebfacf380ea4
Calvin Hawkes <calvinhawkes@gmail.com>
2017-08-05 22:41:54 -0400

    Revert "comparing date times (SLOW AF)"
    This reverts commit 2c7188f2e3b793069aa18ffab360b9e6a43740a8.

--- app/models/flickr_photo.rb
+++ app/models/flickr_photo.rb
@@ -55,7 +55,7 @@ class FlickrPhoto < ApplicationRecord
 
     photos = [] # array of hashs
     FlickrPhoto.select(:id, :lat, :lng, :date_taken).where(city_name: city).each do |p|
-      photos << {lat: p.lat, lng: p.lng, hour: p.date_taken.hour, minute: p.date_taken.strftime('%M')}
+      photos << {lat: p.lat, lng: p.lng, hour: p.date_taken.hour, minute: p.date_taken.min}
     end
 
     tempHash = {
